0040edd47655996171a58a9bdda5dae16e39db6a
Rohald van Merode <rohald_89@live.nl>
2020-12-02 12:11:42 +0100

    added error.pug, page-not-found.pug and created readme file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FSJS-TD-Unit6
+
+This is my sixth project for the FullStack JavaScript Techdegree. For this project I dove into the back-end using Node.js, Express and templating language Pug.
+
+## Features
+- Portfolio displaying all projects created for the FSJS techdegree up to this point
+- Using Pug templates and Express to render the different pages
+
+## Additional Features
+- Personalized styling (buttons, )
+- Navigation to scroll through projects
+- Cleaned up code by adding routes folder and errorHandling.js
+
+## Technologies Used
+- Node.js
+- Express
+- Pug
--- app.js
+++ app.js
@@ -10,16 +10,38 @@ app.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+app.get('/project/:id', (req, res, next) => {
     const id = req.params.id;
     const project = projects[id];
     if (project) {
         res.render('project', { project });
+    } else {
+        const err = new Error('This project can not be found');
+        err.status = 404;
+        next(err);
     }
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.use((req, res, next) => {
+    const err = new Error('This page can not be found');
+    err.status = 404;
+    next(err);
+});
+
+// General catch all error handler to catch any code related errors
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.render('page-not-found.pug', { err });
+    } else {
+        const err = new Error('Uh oh something went wrong!');
+        err.status = 500;
+        res.render('error.pug', { err });
+    }
+
 });
 
 app.listen(3000, () => {
